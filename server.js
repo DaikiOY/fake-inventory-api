ca6ef5182b7e6c875571e5b9c16fdcf76382ac28
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-10-07 15:02:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const jsonServer = require("json-server");
-const path = require("path");
-
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults({ static: "./" });
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(PORT, () => {
-  console.log(`✅ Fake Inventory API running on port ${PORT}`);
+  console.log(`✅ JSON Server is running on port ${PORT}`);
 });
